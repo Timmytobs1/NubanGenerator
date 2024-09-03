1757f3fd6eec69f6853df2c7797d15d22fc801f6
Timmytobs <obadaraoluwatobi12@example.com>
2024-09-03 14:48:15 +0100

    Add project files.

---
+++ NubanGenerator/NubanService.cs
@@ -0,0 +1,30 @@
+﻿namespace NubanGenerator
+{
+    public class NubanService
+    {
+        public int CalculateNubanCheckDigit(string institutionCode, string accountSerialNumber)
+        {
+            if (institutionCode.Length != 6 || accountSerialNumber.Length != 9)
+                throw new ArgumentException("Invalid institution code or account serial number length.");
+
+            string nuban = institutionCode + accountSerialNumber;
+            int[] multipliers = { 3, 7, 3, 3, 7, 3, 3, 7, 3, 3, 7, 3, 3, 7, 3 };
+            int sum = 0;
+
+            for (int i = 0; i < nuban.Length; i++)
+            {
+                sum += (nuban[i] - '0') * multipliers[i];
+            }
+
+            int mod = sum % 10;
+            int checkDigit = 10 - mod;
+            return checkDigit == 10 ? 0 : checkDigit;
+        }
+
+        public string GenerateNuban(string institutionCode, string accountSerialNumber)
+        {
+            int checkDigit = CalculateNubanCheckDigit(institutionCode, accountSerialNumber);
+            return accountSerialNumber + checkDigit.ToString();
+        }
+    }
+}
---
+++ NubanGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NubanGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nubanService = new NubanService();
+
+            string institutionCode = "000011";
+            string accountSerialNumber = "000001456";
+            string nuban = nubanService.GenerateNuban(institutionCode, accountSerialNumber);
+
+            Console.WriteLine($"Generated NUBAN: {nuban}");
+        }
+    }
+}
